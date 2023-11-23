2619baa06433e8280a9faa339a53c8577068b24a
CreatorAndrew <andrew.noah.mitchell@gmail.com>
2023-11-23 13:12:02 -0600

    fix code

--- OneLine.cpp
+++ OneLine.cpp
@@ -1,28 +1,27 @@
 #include <boost/filesystem.hpp>
 #include <sys/stat.h>
 using namespace boost::filesystem;
-using namespace std;
 
-bool command_found(string command) {
+bool command_found(std::string command) {
     struct stat sb;
     #if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__)
-        string delimiter = ";";
+        std::string delimiter = ";";
     #else
-        string delimiter = ":";
+        std::string delimiter = ":";
     #endif
-    string path = string(getenv("PATH"));
+    std::string path = std::string(getenv("PATH"));
     size_t begin = 0, end = 0;
-    while ((end = path.find(":", begin)) != string::npos) {
-        string current_path = path.substr(begin, end - begin) + "/" + command;
+    while ((end = path.find(":", begin)) != std::string::npos) {
+        std::string current_path = path.substr(begin, end - begin) + "/" + command;
         if (stat(current_path.c_str(), &sb) == 0) return true;
         begin = end + 1;
     }
     return false;
 }
 
-string drive(string path) {
-    string letters[] = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
-    string drive = "";
+std::string drive(std::string path) {
+    std::string letters[] = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
+    std::string drive = "";
     for (int i = 0; i < sizeof(letters) / sizeof(letters[0]); i++) {
         drive = " " + letters[i] + ":";
         if ((int) path.find(drive) > -1) break;
@@ -30,20 +29,20 @@ string drive(string path) {
     return drive;
 }
 
-string replace(string orig, string from, string to) {
-    string temp = orig;
+std::string replace(std::string orig, std::string from, std::string to) {
+    std::string temp = orig;
     if ((int) temp.find(from) > -1) temp = temp.replace(temp.find(from), from.length(), to);
     return temp;
 }
 
-string replace_all(string orig, string from, string to) {
-    string temp = orig;
+std::string replace_all(std::string orig, std::string from, std::string to) {
+    std::string temp = orig;
     while ((int) temp.find(from) > -1) temp = replace(temp, from, to);
     return temp;
 }
 
-string substr_up_to(string orig, string substr, int times) {
-    string temp = orig;
+std::string substr_up_to(std::string orig, std::string substr, int times) {
+    std::string temp = orig;
     for (int i = 0; i < times + 1; i++) if ((int) temp.find(substr) > -1) temp = temp.substr(0, temp.rfind(substr));
     temp.append(substr);
     return temp;
@@ -52,13 +51,13 @@ string substr_up_to(string orig, string substr, int times) {
 #if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__)
     #include <Windows.h>
 
-    string wstring_to_string(const wstring& s) {
+    std::string wstring_to_string(const std::wstring& s) {
         int len;
         int slength = (int)s.length() + 1;
         len = WideCharToMultiByte(CP_ACP, 0, s.c_str(), slength, 0, 0, 0, 0);
         char* buf = new char[len];
         WideCharToMultiByte(CP_ACP, 0, s.c_str(), slength, buf, len, 0, 0);
-        string r(buf);
+        std::string r(buf);
         delete[] buf;
         return r;
     }
@@ -67,19 +66,19 @@ string substr_up_to(string orig, string substr, int times) {
         ShowWindow(GetConsoleWindow(), SW_HIDE);
         int argc, once = 0;
         LPWSTR *arg_list = CommandLineToArgvW(GetCommandLineW(), &argc);
-        vector<string> argv;
+        std::vector<std::string> argv;
         for (int i = 0; i < argc; i++) argv.push_back(wstring_to_string(arg_list[i]));
-        string os_drive = "C:/", root_dir = "C:/";
+        std::string os_drive = "C:/", root_dir = "C:/";
 #else
     int main(int argc, char **argv) {
         int once = 1;
-        string os_drive = "Z:/", root_dir = "/";
+        std::string os_drive = "Z:/", root_dir = "/";
 #endif
-    string here = replace_all(system_complete(argv[0]).string(), "\\", "/");
+    std::string here = replace_all(system_complete(argv[0]).string(), "\\", "/");
     #if !(defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__))
         here = "Z:" + here;
     #endif
-    vector<string> args;
+    std::vector<std::string> args;
     for (int i = 1; i < argc; i++) args.push_back(argv[i]);
     bool concat = false, end = false;
     int begin = -1;
@@ -103,7 +102,7 @@ string substr_up_to(string orig, string substr, int times) {
         if (concat) args[begin].append(" ");
     }
     for (int i = 0; i < args.size(); i++) {
-        string line = args[i], prev = "";
+        std::string line = args[i], prev = "";
         int times = once;
         while ((int) line.find("../") > -1) {
             line = replace(line, "../", "");
@@ -113,7 +112,7 @@ string substr_up_to(string orig, string substr, int times) {
         if (prev.empty()) prev = "../";
         line = args[i];
         line = replace_all(replace(replace_all(line, "\\", "/"), prev, substr_up_to(here, "/", times)), "./", substr_up_to(here, "/", once)) + "/";
-        vector<string> segs;
+        std::vector<std::string> segs;
         while ((int) line.find("/") > -1) {
             segs.push_back(line.substr(0, line.rfind("/")));
             segs[segs.size() - 1] = segs[segs.size() - 1].substr(segs[segs.size() - 1].rfind("/") + 1);
@@ -130,7 +129,7 @@ string substr_up_to(string orig, string substr, int times) {
                 }
             }
             if (quote) {
-                string temp = segs[j];
+                std::string temp = segs[j];
                 segs[j] = "\"" + replace(segs[j], drive(segs[j]), "") + "\"";
                 if ((int) temp.find(drive(temp)) > -1) segs[j].append(drive(temp));
             }
@@ -145,7 +144,7 @@ string substr_up_to(string orig, string substr, int times) {
         system(line.c_str());
     }
     #if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__)
-        ShowWindow(GetConsoleWindow(), SW_RESTORE);
+        ShowWindow(GetConsoleWindow(), SW_SHOW);
     #endif
     exit(EXIT_SUCCESS);
 }
